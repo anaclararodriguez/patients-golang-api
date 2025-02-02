79f009ba2b4933a7b4ee2f41a3aacd79e9e3831a
anaclararodriguez <aniirodferr@gmail.com>
2025-02-02 20:58:37 -0300

    added error on db failure

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"patients-golang-api/internal/db"
 	"patients-golang-api/internal/handler"
 )
 
 func main() {
-	db.ConnectDB()
+	err := db.ConnectDB()
+	if err != nil {
+		fmt.Println("Error when trying to connect to DB: ", err.Error())
+	}
 	server := handler.NewServer("8080")
 	server.Start()
 }
